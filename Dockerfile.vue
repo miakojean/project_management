1ae34f7ee8e23c732215ecfa6e9c7c002f00adcc
miakojean <miakojeanyves@gmail.com>
2025-11-29 16:09:13 +0000

    install docker for containering methods project

---
+++ Dockerfile.vue
@@ -0,0 +1,11 @@
+# Dockerfile.vue (version développement uniquement)
+FROM node:20-alpine
+
+WORKDIR /project-management
+COPY frontend/package*.json ./ 
+RUN npm install
+
+COPY project-management .
+
+EXPOSE 5173
+CMD ["npm", "run", "dev"]
